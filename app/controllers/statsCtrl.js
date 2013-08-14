a94b700da037964efed4df837ede30adf1234e6e
Semeon <semeon@my.office>
2013-08-14 15:35:46 +0400

    Stat table blank is ready

--- app/controllers/statsCtrl.js
+++ app/controllers/statsCtrl.js
@@ -9,5 +9,17 @@ function StatsCtrl($scope, $rootScope, ProjectDataLoader) {
 
     $scope.projects = appData.projects;
 
+    $scope.convertDueDate = function(dueDate) {
+      var dueStr = dueDate;
+      if (dueStr == undefined) dueStr = 'No Date';
+      return dueStr;
+    }
+
+    $scope.getTrackerName = function(trackerId) {
+      var trackerName = '';
+      console.log(log_ctrl + 'trackerId: ' + trackerId);
+      trackerName = redmineSettings.issueTrackersMap[String(trackerId)].name;
+      return trackerName;
+    }
 
 }
