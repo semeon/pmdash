ace34c693dbb54a0f5af3bc7effb55b114f6a821
Semeon <semeon@my.office>
2013-08-27 18:57:27 +0400

    Service refactored

--- app/services/burnDownChartSrvc.js
+++ app/services/burnDownChartSrvc.js
@@ -6,35 +6,26 @@
 
     var chartService = {};
 
-
-
-    function resetChartData() {
-        var chartData = {};
-        chartData.labels = [];
-        chartData.datasets = [];
-        chartData.datasets[0] = {};
-        chartData.datasets[0].data = [];
-        chartData.datasets[0].fillColor =        "rgba(220,220,220,0.0)";
-        chartData.datasets[0].strokeColor =      "rgba(220,220,220,1)";
-        chartData.datasets[0].pointColor =       "rgba(220,220,220,1)";
-        chartData.datasets[0].pointStrokeColor = "#fff";
-
-        chartData.datasets[1] = {};
-        chartData.datasets[1].data = [];
-        chartData.datasets[1].fillColor =        "rgba(60,100,255,0.3)";
-        chartData.datasets[1].strokeColor =      "rgba(60,100,255,1)";
-        chartData.datasets[1].pointColor =       "rgba(60,100,255,1)";
-        chartData.datasets[1].pointStrokeColor = "#f0f0f0";
-
-        return chartData;
-    }
-
-
-    chartService.create = function (project, version, data) { 
-
-        var chartData = resetChartData();
+    function Chart(data) {
         var yaxisMax = 0;
         var duration = 20;
+
+        this.data = {};
+        this.data.labels = [];
+        this.data.datasets = [];
+        this.data.datasets[0] = {};
+        this.data.datasets[0].data = [];
+        this.data.datasets[0].fillColor =        "rgba(220,220,220,0.0)";
+        this.data.datasets[0].strokeColor =      "rgba(220,220,220,1)";
+        this.data.datasets[0].pointColor =       "rgba(220,220,220,1)";
+        this.data.datasets[0].pointStrokeColor = "#fff";
+
+        this.data.datasets[1] = {};
+        this.data.datasets[1].data = [];
+        this.data.datasets[1].fillColor =        "rgba(60,100,255,0.3)";
+        this.data.datasets[1].strokeColor =      "rgba(60,100,255,1)";
+        this.data.datasets[1].pointColor =       "rgba(60,100,255,1)";
+        this.data.datasets[1].pointStrokeColor = "#f0f0f0";
 
         for (var i=1; i<duration; i++) {
             var date = '08.';
@@ -42,41 +33,45 @@
               date += '0';
             }
             date += i;
-            chartData.labels.push(date);
+            this.data.labels.push(date);
 
             if (data.records[date]) {
                 var value = data.records[date].value;
-                chartData.datasets[1].data.push(value);
+                this.data.datasets[1].data.push(value);
                 if(value > yaxisMax) yaxisMax = value;
             }
         }
 
-        var yaxisStep = Math.round(yaxisMax/10);
+        yaxisStep = Math.round(yaxisMax/10);
 
         // Ideal line
         var idealStep = yaxisMax/ (duration-3);
-
-        var nextValue = yaxisMax;
-
-        for (var j=1; j<duration; j++) {
-            chartData.datasets[0].data.push(nextValue);
-            nextValue = nextValue - idealStep;
+        for (var j=0; j<duration; j++) {
+            this.data.datasets[0].data.push(yaxisMax - idealStep*j);
         }
 
-
-
-        var modalScope = $rootScope.$new(true);
-        modalScope.title = 'Burndown chart: ' + project.title + ' / ' + version.name;
-        modalScope.due_date = version.due_date;
-        modalScope.data = chartData;
-        modalScope.chartSettings =  {
-            scaleOverride : true,
+        this.settings = {
+            scaleOverride: true,
             scaleSteps: yaxisMax*1.1/yaxisStep,
             scaleStepWidth: yaxisStep,
             scaleStartValue: 0,
             scaleGridLineWidth: 1,
             bezierCurve : false
         };
+    }
+
+
+
+    chartService.create = function (project, version, data) { 
+
+        var chart = new Chart(data);
+
+        var modalScope = $rootScope.$new(true);
+        modalScope.title = 'Burndown chart: ' + project.title + ' / ' + version.name;
+        modalScope.due_date = version.due_date;
+
+        modalScope.data = chart.data;
+        modalScope.chartSettings =  chart.settings;
 
         var modalTitle = 'Burndown chart: ' + project.title + ' / ' + version.name;
         var modalBody = '';
